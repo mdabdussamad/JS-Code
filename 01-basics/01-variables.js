3f6e7ae2469eea991020eb6d1ef0c535736b21d3
Md. Abdussamad <30697752+mdabdussamad@users.noreply.github.com>
2023-11-23 06:31:30 +0000

    variable

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -2,13 +2,21 @@ var accountPassword = "54321"
 let accountEmail = "mdabdussamad33@gmail.com"
 const accountId = 12356
 accountCity = "Birgunj"
-
 let accountState;
 
 accountPassword = "333333" 
 accountEmail = "test@gmail.com"
-// accountId = 2 
-accountCity = "Kathmandu"
+// accountId = 5 
 
-// console.log(accountId);
-console.log([accountPassword,accountEmail,accountId,accountCity]);
+/* Note: This is not allow because it is define as const. */
+accountCity = "Kathmandu";
+
+//console.log(accountId);
+
+console.table([accountPassword, accountEmail,accountId, accountCity]);
+
+/*
+Note: It is better to not use var
+because of issue in block scope 
+and functional scope
+*/
