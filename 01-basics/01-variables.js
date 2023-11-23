88bae0a9c2888d194ea5adae0bdde182151929c7
Md. Abdussamad <30697752+mdabdussamad@users.noreply.github.com>
2023-11-23 05:34:10 +0000

    var-let-const

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+var accountPassword = "54321"
+let accountEmail = "mdabdussamad33@gmail.com"
+const accountId = 12356
+accountCity = "Birgunj"
+
+let accountState;
+
+accountPassword = "333333" 
+accountEmail = "test@gmail.com"
+// accountId = 2 
+accountCity = "Kathmandu"
+
+// console.log(accountId);
+console.log([accountPassword,accountEmail,accountId,accountCity]);
